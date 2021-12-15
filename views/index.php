b0fbb6bd013d9bdf9b947f1695628a732f6ef005
Tinho Junior <brahima.sanogo79@gmail.com>
2021-12-15 14:55:14 +0000

    Ajout de la première partie du site.

---
+++ views/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mairie de Man</title>
+</head>
+<body>
+    <h1>C'EST SENSÉ ÊTRE LA PAGE D'ACCUEIL </h1>
+</body>
+</html>
